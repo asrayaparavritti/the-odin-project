8637d59da7a4f45246780a523cd7f6ef65df5672
asraya <212255901+asrayaparavritti@users.noreply.github.com>
2026-04-30 15:28:32 -0300

    resolucao do exercicio 03_numberChecker copm logica correta que eu nao havia entendido

--- javascript-exercises/foundations/03_numberChecker/numberChecker.js
+++ javascript-exercises/foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,6 @@
 function numberChecker(number) {
-  if (number === 1000 || number === 10 ) {
+  if (number >=10 ) {
     return true;
-  } else if (number === 9) {
-    return false;
-  } else if (number === 6) {
-    return false;
   } else {
     return false;
   }
