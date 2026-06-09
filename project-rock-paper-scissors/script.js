e467f308f34acb5f567047e90593306af72dfe0e
asraya <212255901+asrayaparavritti@users.noreply.github.com>
2026-06-09 14:29:49 -0300

    adiciona um codigo feito por mim apos repetir algumas vezes e fazer sozinho

--- project-rock-paper-scissors/script.js
+++ project-rock-paper-scissors/script.js
@@ -1,73 +1,75 @@
-function getComputerChoice (max) {
+function getComputerChoice (max){
 
-    let getCPU = Math.floor(Math.random() * max );
+    let computerChoice = Math.floor(Math.random() * max);
 
-    if (getCPU === 0) {
-        getCPU = "pedra";
-    } else if (getCPU === 1) {
-        getCPU = "papel";
-    } else if (getCPU === 2) {
-        getCPU = "tesoura";
-    }
+    if (computerChoice === 0){
+        computerChoice = "tesoura";
+    } else if (computerChoice === 1){
+        computerChoice = "pedra";
+    } else if (computerChoice === 2){
+        computerChoice = "papel";
+    } return computerChoice;
 
-    return getCPU;
 }
 
-function getHumanChoice () {
-    let getUSER = window.prompt("Escolha Pedra, Papel ou Tesoura: ");
-    getUSER = getUSER.toLowerCase();
+function getHumanChoice (){
 
-    return getUSER;
-};
+    let userChoice = window.prompt("Escolha Pedra, Papel ou Tesoura: ");
+    return userChoice.toLowerCase();
 
+}
 
-function playGame () {
+function playGame (){
 
-    let userScore = 0;
     let computerScore = 0;
+    let userScore = 0;
 
-    function playRound (userChoice, computerChoice) {
+    function playRound (userChoice, computerChoice){
 
-    if (userChoice === computerChoice) {
+        if (userChoice === computerChoice){
+            console.log("Empate!");
+        } else if (userChoice === "pedra" && computerChoice === "tesoura" ||
+            userChoice === "papel" && computerChoice === "pedra" ||
+            userChoice === "tesoura" && computerChoice === "papel"
+        ){
+            console.log("Você ganhou!");
+            userScore++;
+        } else if (userChoice === "tesoura" && computerChoice === "pedra" ||
+            userChoice === "pedra" && computerChoice === "papel" ||
+            userChoice === "papel" && computerChoice === "tesoura"
+        ){
+            console.log("Você perdeu!");
+            computerScore++;
+        }
 
-        console.log("Empate!");
-
-    } else if (
-
-        (userChoice === "pedra" && computerChoice === "tesoura") || 
-        (userChoice === "tesoura" && computerChoice === "papel") || 
-        (userChoice === "papel" && computerChoice === "pedra")) 
-        
-    {
-        console.log("Você ganhou!");
-        userScore++;
-
-    } else {
-        console.log("Você perdeu!");
-        computerScore++;
     }
-             
-}   
-    console.log("--- RODADA 1 ---");
-    playRound(getHumanChoice(), getComputerChoice(3));
-    console.log(`Placar atual - Você: ${userScore} | PC: ${computerScore}`);
 
-    console.log("--- RODADA 2 ---");
-    playRound(getHumanChoice(), getComputerChoice(3));
-    console.log(`Placar atual - Você: ${userScore} | PC: ${computerScore}`);
+    console.log("Rodada 1");
 
-    console.log("--- RODADA 3 ---");
     playRound(getHumanChoice(), getComputerChoice(3));
-    console.log(`Placar atual - Você: ${userScore} | PC: ${computerScore}`);
+    console.log(`Pontuação do usuario:${userScore}, e pontuação do Computador:${computerScore}`)
 
-    console.log("--- RODADA 4 ---");
-    playRound(getHumanChoice(), getComputerChoice(3));
-    console.log(`Placar atual - Você: ${userScore} | PC: ${computerScore}`);
+    console.log("Rodada 2");
 
-    console.log("--- RODADA 5 ---");
     playRound(getHumanChoice(), getComputerChoice(3));
-    console.log(`Placar Final - Você: ${userScore} | PC: ${computerScore}`);
+    console.log(`Pontuação do usuario:${userScore}, e pontuação do Computador:${computerScore}`);
+
+    console.log("Rodada 3");
+
+    playRound(getHumanChoice(), getComputerChoice(3));
+    console.log(`Pontuação do usuario:${userScore}, e pontuação do Computador:${computerScore}`);
+
+    console.log("Rodada 4");
+
+    playRound(getHumanChoice(), getComputerChoice(3));
+    console.log(`Pontuação do usuario:${userScore}, e pontuação do Computador:${computerScore}`);
+
+    console.log("Rodada 5");
+
+    playRound(getHumanChoice(), getComputerChoice(3));
+    console.log(`Pontuação do usuario:${userScore}, e pontuação do Computador:${computerScore}`);
+
+
 }
-
 
 playGame();
